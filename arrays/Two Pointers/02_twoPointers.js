7e646afade3de5f0dcde2a7bbce19d614fe7390f
shreyashtalele <taleleshreyash44@gmail.com>
2026-08-06 14:52:03 +0530

    'two-pointers-same-direction-complete'

--- arrays/Two Pointers/02_twoPointers.js
+++ arrays/Two Pointers/02_twoPointers.js
@@ -15,3 +15,100 @@ function removeDuplicate(array) {
 }
 
 console.log(removeDuplicate([1, 1, 2, 2, 3, 4, 4, 5]));
+
+
+
+function moveZeros(array) {
+    let read = 0;
+    let write = 0;
+
+    while (read < array.length) {
+        if (array[read] !== 0) {
+            array[write] = array[read]
+            write++
+        }
+        read++
+    }
+
+    while (write < array.length) {
+        array[write] = 0
+        write++
+    }
+
+    return array
+}
+
+console.log(moveZeros([0, 10, 0, 20, 30, 0, 40]));
+
+
+function removeAllOccurence(array, target) {
+    let read = 0
+    let write = 0
+    while (read < array.length) {
+        if (array[read] !== target) {
+            array[write] = array[read]
+            write++
+        }
+        read++
+    }
+    array.length = write
+    return array
+}
+
+console.log(removeAllOccurence([3, 3, 2, 3, 4, 5, 6], 3));
+
+
+function removeEvenNumbers(array) {
+    let read = 0
+    let write = 0
+
+    while (read < array.length) {
+        if (array[read] % 2 !== 0) {
+            array[write] = array[read]
+            write++
+        }
+        read++
+    }
+
+    array.length = write
+    return array
+
+}
+console.log(removeEvenNumbers([2, 4, 6, 7, 8, 10]));
+
+
+function removeNegativeNumbers(array) {
+    let read = 0;
+    let write = 0;
+
+    while (read < array.length) {
+        if (array[read] >= 0) {
+            array[write] = array[read]
+            write++
+        }
+        read++
+    }
+
+    array.length = write
+    return array
+}
+
+console.log(removeNegativeNumbers([-2, 4, -1, 8, -5]));
+
+
+function onlyEvenNumbers(array) {
+    let read = 0;
+    let write = 0;
+
+    while (read < array.length) {
+        if (array[read] % 2 === 0) {
+            array[write] = array[read]
+            write++
+        }
+        read++
+    }
+    array.length = write
+    return array
+}
+
+console.log(onlyEvenNumbers([1, 2, 3, 4, 5, 6]));
